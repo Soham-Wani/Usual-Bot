a8b43c85a520fee7c2e54d517f9b4c91820c4a41
Soham Wani <68885526+SohamWani@users.noreply.github.com>
2022-02-18 22:34:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //https://discord.com/api/oauth2/authorize?client_id=928874082756345917&permissions=537390082&scope=bot
-//Todo: chnage , with prefix
 const mySecret = process.env['DISCORD_TOKEN'];
 const Discord = require('discord.js');
 const client = new Discord.Client({
