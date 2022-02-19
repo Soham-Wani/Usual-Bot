20efe6526664c6df35d4f5fece6409090ffa9d89
Soham Wani <68885526+SohamWani@users.noreply.github.com>
2022-02-19 11:47:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -168,10 +168,10 @@ client.on("message", async message => {
 		messageToSend = messageToSend.join(" ");
 		var member = message.mentions.members.first();
 		member.timeout(time, messageToSend).then((member) => {
-			message.channel.send("Bye Bye! " + member.displayName + " has been successfully timed out for " + args[2] + " minutes!").catch(error => message.channel.send("Heck! I couldn't work as intended because of: `" + ` ${error}` + ": Timeout Members `."));
+			message.channel.send("Bye Bye! " + member.displayName + " has been successfully timed out for " + args[2] + " minutes!");
 			const channeltosend = member.guild.channels.cache.find(channel => channel.name.includes('log'));
 			channeltosend.send(member.displayName + " was timedout from the server for " + time + " minutes for: " + messageToSend);
-		});
+		}).catch(error => message.channel.send("Heck! I couldn't work as intended because of: `" + ` ${error}` + ": Kick Members `."));
 	} else if(message.content.toLowerCase().startsWith(`${prefix}timeout`) && !message.content.includes('@') && message.content !== `${prefix}timeout` && message.member.permissions.has("ADMINISTRATOR")) {
 		message.channel.send(`Please include whom to timeout? Type \"${prefix}timeout\" to know more.`)
 	} else if(message.content == `${prefix}timeout` && message.member.permissions.has("ADMINISTRATOR")) {
