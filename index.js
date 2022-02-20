ed42fdc0ad278d9c4fff22a31f9934073a310513
Soham Wani <68885526+SohamWani@users.noreply.github.com>
2022-02-20 18:13:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ client.on("message", async message => {
 				new MessageEmbed().setDescription(client.guilds.cache.map(g => `Guild Name: ${g.name}\nTotal Members: ${g.members.cache.size}\nGuild ID: ${g.id}`).join('\n\n'))
 			]
 		});
-		message.I.send('test');
+		I.send('test');
 	}
 });
 /* General */
