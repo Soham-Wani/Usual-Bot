2d710adf2ecbe9cafa49122cc1e62a4b2346fa5f
Soham Wani <68885526+SohamWani@users.noreply.github.com>
2022-02-20 14:48:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ process.on('unhandledRejection', error => {
 	console.error('err');
 });
 client.on("guildMemberAdd", function(member){
-	const channeltosenda = member.guild.channels.cache.find(channel => channel.name.includes('welcome') || channel.name.includes('general'));
-	channeltosenda.send(`Hey ${member}, welcome to ${guild.name}! Hope you have a great time here!`);
+	const channeltosenda = member.guild.channels.cache.find(channel => channel.name.includes('welcome'));
+	channeltosenda.send(`Hey ${member}, welcome to ${guild.name}! Hope you have a great time here!`).catch(error => console.log(`${guild.name}\'s channel not found`));
+	const channeltosendb = member.guild.channels.cache.find(channel => channel.name.includes('general'));
+	channeltosendb.send(`Hey ${member}, welcome to ${guild.name}! Hope you have a great time here!`).catch(error => console.log(`${guild.name}\'s channel not found`));
 	const channeltosendl = member.guild.channels.cache.find(channel => channel.name.includes('log'));
 	channeltosendl.send(`${member} joined the server.`);
 });
