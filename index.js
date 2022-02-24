0d3ac7c9ef5af88ed79f6e515a29a486c6284866
Soham Wani <68885526+SohamWani@users.noreply.github.com>
2022-02-24 23:44:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -223,8 +223,8 @@ client.on("message", async message => {
                 channeltosend.send(member.displayName + " was kicked from the server for: " + messageToSend);
             }).catch(error => message.channel.send("Heck! I couldn't work as intended because of: `" + ` ${error}` + ": Kick Members `."));
         }
-    } else if (message.content.toLowerCase().startsWith(`${prefix}kick`) && !message.content.includes('@') && message.content !== `${prefix}kick` && message.member.permissions.has("ADMINISTRATOR")) {
-        message.channel.send(`Please include whom to kick? Type __${prefix}kick__ to know more.`)
+    } else if (message.content.toLowerCase().startsWith(`${prefix}kick`) && (!message.content.includes('@') || !message.mentions.members.first()) && message.content !== `${prefix}kick` && message.member.permissions.has("ADMINISTRATOR")) {
+        message.channel.send(`Please include a valid user Type __${prefix}kick__ to know more.`)
     } else if (message.content.toLowerCase() == `${prefix}kick` && message.member.permissions.has("ADMINISTRATOR")) {
         const kickEmbed = new MessageEmbed().setColor('#0c0c66').setTitle(`Kick (${prefix}kick)`).setDescription(`Using the ${prefix}kick command allows people with Administrator permissions to kick members easily.\n\nTyping __${prefix}kick @person reason__ will kick that person for mentioned reason.`);
         message.channel.send({
