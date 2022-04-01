c8ad78ebcd5760575e3b31e2e62a7306623e1385
Soham Wani <68885526+SohamWani@users.noreply.github.com>
2022-04-01 15:41:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.on("guildCreate", guild => {
         });
     });
 });
-client.on('messageDelete', function(message) {
+client.on('messageDelete', async msg {
 let logs = await msg.guild.fetchAuditLogs({type: 72});
   let entry = logs.entries.first();
 
