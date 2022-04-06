da45198e671bd76090c71cc7760b73aae9b6f9b7
Soham Wani <68885526+SohamWani@users.noreply.github.com>
2022-04-06 21:49:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -388,4 +388,17 @@ client.on("message", async message => {
             message.delete().then((msg) => msg.channel.send(`${user} Buying ads don't belong here!`));
         }
     }
+const opponent = message.mentions.users.first();
+if (!opponent) return message.channel.send(`Please mention who you want to challenge at tictactoe.`);
+const { TicTacToe } = require('djs-games')
+const game = new TicTacToe({
+  message: message,
+  opponent: opponent,
+  xEmoji: '❌', // The Emote for X
+  oEmoji: '0️⃣', // The Emote for O
+  xColor: 'PRIMARY',
+  oColor: 'PRIMARY', // The Color for O
+  embedDescription: 'Tic Tac Toe', // The Description of the embed
+})
+game.start()
 });
